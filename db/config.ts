edd719489c6f03b878633e366933b89b7dc16972
Francisco Aquino <franciscoaquino19@gmail.com>
2024-04-02 00:58:32 -0300

    Add 'created' column to Contact table and remove unnecessary code in Contact.astro

--- db/config.ts
+++ db/config.ts
@@ -1,11 +1,12 @@
-import { defineDb, defineTable, column } from 'astro:db'
+import { defineDb, defineTable, column, NOW } from 'astro:db'
 
 const Contact = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
     name: column.text(),
     email: column.text(),
-    message: column.text()
+    message: column.text(),
+    created: column.date({ default: NOW })
   }
 })
 
